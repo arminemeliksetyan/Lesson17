a4ceb0287852e44f652fffba81a5c762966c25f3
armine.meliksetyan.1996@gmail.com <armine.meliksetyan.1996@gmail.com>
2023-07-13 17:51:26 +0400

    registration form wpf, calculiator

---
+++ RegistrationWpf/DataBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationWpf
+{
+    internal class DataBase
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Server=DESKTOP-QCOA7R0\SQLEXPRESS;Database=first_db;Trusted_Connection=True;");
+        public void OpenConnection() 
+        { 
+            if(sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public SqlConnection GetConnection() 
+        {
+            return sqlConnection;
+        }
+    }
+}
---
+++ RegistrationWpf/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RegistrationWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataBase dataBase = new DataBase();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            password.MaxLength = 50;
+            login.MaxLength = 50;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var loginUser = login.Text.Trim();
+            var passwordUser = password.Text.Trim();
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable dataTable = new DataTable();
+
+            string queryString = $"select id, login_user, password_user from first_db login_user = '{loginUser}' and password_user = '{passwordUser}'";
+            SqlCommand command = new SqlCommand(queryString, dataBase.GetConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(dataTable);
+
+            if(dataTable.Rows.Count == 1)
+            {
+                MessageBox.Show("Sucsess", " wow", MessageBoxButton.OK);
+            }
+        }
+    }
+}
